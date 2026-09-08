43c505e35eb303d1103ebdfc9a4c26ccb5b371fa
oodwebserv01 <67729642+oodwebserv01@users.noreply.github.com>
2026-09-08 10:31:27 +0700

    Delete java directory

--- java/xmlFastParser/xmlBluePrintCall.java
+++
@@ -1,5 +0,0 @@
-
-public interface xmlBluePrintCall {
-    // Original method for backward compatibility
-    public boolean call(Object idToken, xmlBluePrintHolder holder, int event, int nameBegin, int nameEnd, int valueBegin, int valueEnd);
-}
